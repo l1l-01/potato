1bf819664c2a1f76d195dc7c42bec14222dc7027
Khalil <khalil23ar@gmail.com>
2026-05-11 20:18:18 +0100

    feat(parser): started parser, added error handling for missing or multiple CRUD operators, improved lexer with additional token types for DSL context

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { lexer } from "./potato.js";
+import { parser } from "./potato.js";
 
 function getById(id) {
   return document.querySelector(id);
@@ -13,18 +13,5 @@ const BTN = getById("#btn");
 
 addEvent(BTN, "click", () => {
   let VALUE = INPUT.value;
-  lexer(VALUE);
+  parser(VALUE);
 });
-/*
-if (INPUT.value.includes(";")) {
-  for (let key in TOKENS) {
-    if (value.includes(TOKENS[key])) {
-      instruction += TOKENS[key];
-      console.log(instruction);
-    }
-  }
-} else {
-  value = input.value;
-  console.log("Still");
-}
-  */
--- js/potato.js
+++ js/potato.js
@@ -1,33 +1,42 @@
 const KEYWORDS = {
-  LET: "KEYWORD",
-  SET: "KEYWORD",
-  GET: "KEYWORD",
-  UPD: "KEYWORD",
-  DLT: "KEYWORD",
-  PUT: "KEYWORD",
-  DROP: "KEYWORD",
   ASEC: "KEYWORD",
   DESC: "KEYWORD",
   ALL: "KEYWORD",
+  AND: "KEYWORD",
+  OR: "KEYWORD",
+  WHERE: "KEYWORD",
+  BETWEEN: "KEYWORD",
+};
+
+const ACTIONS = {
+  LET: "ACTION",
+  GET: "ACTION",
+  UPD: "ACTION",
+  DLT: "ACTION",
+  PUT: "ACTION",
+  DROP: "ACTION",
+};
+
+const DATATYPES = {
   STR: "DATATYPE",
   INT: "DATATYPE",
   DEC: "DATATYPE",
   BOOL: "DATATYPE",
   DATE: "DATATYPE",
   ENUM: "DATATYPE",
-  AND: "LOGICAL",
-  OR: "LOGICAL",
-  WHERE: "LOGICAL",
-  BETWEEN: "LOGICAL",
-  SUM: "FUNCTION",
-  AVG: "FUNCTION",
-  COUNT: "FUNCTION",
 };
 
 const PREFIXES = {
   ":": "VALUE",
   "@": "LIMITAION",
   "#": "IDENTIFICATION",
+  $: "FIELD",
+};
+
+const FUNCTIONS = {
+  SUM: "KEYWORD",
+  AVG: "KEYWORD",
+  COUNT: "KEYWORD",
 };
 
 const OPERATORS = {
@@ -38,7 +47,15 @@ const OPERATORS = {
   "<": "OPERATOR",
 };
 
-export function lexer(VALUE) {
+const ERROR_TYPES = {
+  CRUD_KEYWORD:
+    "Error(001): Missing CRUD keyword. An action must be specified. Use one of: LET, GET, UPD, DLT.",
+  CRUD_OPERATIONS:
+    "Error(002): Only one CRUD operation can run at a time. You are trying to use more than one: ",
+  CRUD_KEYWORD_MISPLACED: "ERROR(003): Misplaced CRUD keyword: ",
+};
+
+function lexer(VALUE) {
   let tokens = [];
   let type = null;
   const MAIN_RE = new RegExp(
@@ -47,8 +64,11 @@ export function lexer(VALUE) {
   );
   const SUB_RE = new RegExp(`\\${Object.keys(OPERATORS).join("|")}\\i`, "gi");
 
-  const words = VALUE.split(/(\s+)/);
-  words.forEach((word, i) => {
+  // * Split user input into an array on whitespace
+  const WORDS = VALUE.split(/(\s+)/);
+
+  //* Create tokens: for each word in the words array, record type, value, and position
+  WORDS.forEach((word, i) => {
     word = word.trim();
     if (!MAIN_RE.test(word)) {
       if (word) {
@@ -63,8 +83,22 @@ export function lexer(VALUE) {
           }
         }
 
+        for (let key in ACTIONS) {
+          if (word.includes(key)) {
+            type = ACTIONS[key];
+            word = word;
+          }
+        }
+
+        for (let key in DATATYPES) {
+          if (word.includes(key)) {
+            type = DATATYPES[key];
+            word = key;
+          }
+        }
+
         if (type != type.toUpperCase()) {
-          tokens.push({ type: "IDENTIFIER", value: word, position: i });
+          tokens.push({ type: "TABLE", value: word, position: i });
         } else {
           tokens.push({ type: type, value: word, position: i });
         }
@@ -77,5 +111,95 @@ export function lexer(VALUE) {
     }
   });
 
+  console.log(tokens);
+
   return tokens;
+}
+
+export function parser(VALUE) {
+  const TOKENS = lexer(VALUE);
+  let errors = [];
+  let fields = [];
+  let values = [];
+  const atsNode = {
+    action: "",
+    type: "",
+    table: "",
+    column: [],
+  };
+
+  //* Catch missing CRUD operator && Detect multiple CRUD operations used simultaneously
+  const actions =
+    TOKENS.filter(
+      (token) =>
+        token.value == "LET" ||
+        token.value == "GET" ||
+        token.value == "UPD" ||
+        token.value == "DLT" ||
+        token.value == "DROP",
+    ) || null;
+
+  const ACTIONS_LENGTH = actions.length;
+
+  if (ACTIONS_LENGTH == 0) {
+    errors.push(ERROR_TYPES.CRUD_KEYWORD);
+    return errors;
+  } else if (ACTIONS_LENGTH >= 2) {
+    let operations = "";
+    actions.forEach((action, i) => {
+      operations += `${action.value} at position ${action.position}${ACTIONS_LENGTH == i + 1 ? "." : ", "}`;
+    });
+    const ERROR = ERROR_TYPES.CRUD_OPERATIONS + operations;
+    errors.push(ERROR);
+    return errors;
+  }
+
+  TOKENS.forEach((token, i) => {
+    if (token.type != "TABLE") {
+      for (let keyword in KEYWORDS) {
+        if (token.type == "KEYWORD" && keyword == token.value) {
+          console.log("Keyword: ", keyword, token.type, i);
+        }
+      }
+
+      for (let keyword in DATATYPES) {
+        if (token.type == "DATATYPE" && keyword == token.value) {
+          console.log("Datatype", keyword, token.type, i);
+        }
+      }
+
+      for (let keyword in OPERATORS) {
+        if (token.type == "OPERATOR" && keyword == token.value) {
+          console.log("Operator", keyword, token.type, i);
+        }
+      }
+    } else {
+      //* Get table name and fields names
+      const TOKEN_VALUE = TOKENS[i - 1]?.value;
+      if (TOKEN_VALUE != undefined) {
+        if (
+          TOKEN_VALUE == "LET" ||
+          TOKEN_VALUE == "GET" ||
+          TOKEN_VALUE == "UPD" ||
+          TOKEN_VALUE == "DLT" ||
+          TOKEN_VALUE == "DROP"
+        ) {
+          console.log("Table Name: ", token.value, i);
+          atsNode.table = token.value;
+        } else {
+          fields.push({ field: token.value, filed_position: token.position });
+        }
+      } else {
+        const ERROR = `${ERROR_TYPES.CRUD_KEYWORD_MISPLACED} ${actions[0].value} at position ${actions[0].position}.`;
+        errors.push(ERROR);
+      }
+      console.log("TABLE", token.type, token.value, i);
+    }
+  });
+
+  /*
+  const error = `Error: The OPERATOR ${token.value} was written wrong at the position ${token.position}, It should be writen like ${keyword}`;
+  errors.push(error);
+  */
+  console.log(atsNode, errors, fields);
 }
